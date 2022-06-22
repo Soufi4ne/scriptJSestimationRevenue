af2345ebb77f2c6e6566d76b52b1090525a4fd1f
Soufi4ne <soufiane.le.breton@efrei.net>
2022-06-22 13:27:02 +0200

    update script version BETA

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let region = document.querySelectorAll('selectionRegion');
 let type = document.querySelectorAll('selectionType');
-let selectionPlace = document.querySelectorAll('selectionPlace');
+let place = document.querySelectorAll('selectionPlace');
 // ------------------------------------------------
 let idf = document.querySelectorAll('IleDeFrance');
 let bzh = document.querySelectorAll('Bretagne')
@@ -10,7 +10,7 @@ let corse = document.querySelectorAll("Corse")
 console.log(bzh)
 // -------------------------------------
 
-region.onchange() = function(valeurRegion) {
+let fonctionRegion = region.onchange() = function(valeurRegion) {
     valeurRegion=0;
     if (value=='IleDeFrance') {
         valeurRegion == 300
@@ -21,5 +21,30 @@ region.onchange() = function(valeurRegion) {
     else {
         valeurRegion == 200
     }
+    console.log(valeurRegion)
+}
+
+let fonctionType = type.onchange() = function (valeurType) {
+    valeurType = 0
+    if (value=="Domicile") {
+        valeurType == 150
+    } else {
+        valeurType == 300
+    }
+    console.log(valeurType)
+}
+
+let fonctionPlace = place.onchange() = function(valeurPlace) {
+    valeurPlace=0;
+    if (value=='1a2') {
+        valeurPlace== 50
+    }
+    else if (value == '2a4') {
+        valeurPlace == 100  
+    } 
+    else {
+        valeurPlace == 150
+    }
+    console.log(valeurRegion)
 }
 
