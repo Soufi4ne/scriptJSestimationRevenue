5c753e805d08ae0e1dc94f0777c5b74c5e9c9f9e
Soufi4ne <soufiane.le.breton@efrei.net>
2022-06-22 13:11:55 +0200

    update script version alpha

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-let region = document.getElementById('selectionRegion');
-let type = document.getElementById('selectionType');
-let selectionPlace = document.getElementById('selectionPlace');
+let region = document.querySelectorAll('selectionRegion');
+let type = document.querySelectorAll('selectionType');
+let selectionPlace = document.querySelectorAll('selectionPlace');
+// ------------------------------------------------
+let idf = document.querySelectorAll('IleDeFrance');
+let bzh = document.querySelectorAll('Bretagne')
+let normandie = document.querySelectorAll("Normandie")
+let corse = document.querySelectorAll("Corse")
+// alert(bzh)
+console.log(bzh)
+// -------------------------------------
+
+region.onchange() = function(valeurRegion) {
+    valeurRegion=0;
+    if (value=='IleDeFrance') {
+        valeurRegion == 300
+    }
+    else if (value == 'corse') {
+        valeurRegion == 250
+    } 
+    else {
+        valeurRegion == 200
+    }
+}
 
