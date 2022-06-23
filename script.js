e4bc303b4a471046b845729f11937d754bec2211
Soufi4ne <soufiane.le.breton@efrei.net>
2022-06-23 17:20:10 +0200

    script version ultime

--- script.js
+++ script.js
@@ -6,54 +6,62 @@ var value = null;
 var type = null;
 
 function fonctionArgent(event) {
-    console.log("hello");
-    value = event.value;
-    type = event.id;
-    console.log(event.id);
+  console.log("hello");
+  value = event.value;
+  type = event.id;
+  console.log(event.id);
 
-    if (type === 'selectionRegion') {
-        switch (value) {
-            case 'IleDeFrance':
-                valeurRegion = 300
-                break;
-            case 'Corse':
-                valeurRegion = 250
-                break;
-            case 'empty':
-                valeurRegion = 0
-                break;
-            default:
-                valeurRegion = 200 
-        }
-    
-    } 
-    else if (type == "selectionType") {
-        console.log("modif type");
-        if (value == "Domicile") {
-            valeurType = 150
-        } else {
-            valeurType = 300
-        }
-    } if (type === 'selectionPlace') {
-        switch (value) {
-            case '1a2':
-                valeurPlace = 100
-                break;
-            case '2a4':
-                valeurPlace = 200
-                break;
-            case '4Plus':
-                valeurPlace = 300
-                break;
-            default:
-                valeurPlace = 0
-        }
-    
-    } 
+  if (type === "selectionRegion") {
+    switch (value) {
+      case "IleDeFrance":
+        valeurRegion = 300;
+        break;
+      case "Corse":
+        valeurRegion = 250;
+        break;
+      case "Bretagne":
+        valeurRegion = 200;
+        break;
+      case "Normandie":
+        valeurRegion = 150;
+        break;
+      default:
+        valeurRegion = 0;
+    }
+  }
 
-    console.log(valeurPlace + " + " + valeurType + " + " + valeurRegion);
-    globalvalue = valeurPlace + valeurType + valeurRegion;
-    console.log(globalvalue);
-    document.querySelector("#estimation").innerHTML = "Jusqu'à " + `<span style='color: #ec5075;'> ${globalvalue}<span/>`  + "€";
+  if (type === "selectionType") {
+    switch (value) {
+      case "Domicile":
+        valeurType = 100;
+        break;
+      case "Logement":
+        valeurType = 200;
+        break;
+      default:
+        valeurType = 0;
+    }
+  }
 
+  if (type === "selectionPlace") {
+    switch (value) {
+      case "1a2":
+        valeurPlace = 100;
+        break;
+      case "2a4":
+        valeurPlace = 200;
+        break;
+      case "4Plus":
+        valeurPlace = 300;
+        break;
+      default:
+        valeurPlace = 0;
+    }
+  }
+
+  console.log(valeurPlace + " + " + valeurType + " + " + valeurRegion);
+  globalvalue = valeurPlace + valeurType + valeurRegion;
+  console.log(globalvalue);
+  document.querySelector("#estimation").innerHTML =
+    "Jusqu'à " + `<span style='color: #ec5075;'> ${globalvalue}<span/>` + "€";
 }
